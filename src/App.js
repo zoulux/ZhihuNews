38c2f6602ae8a33a133c5c41fbc4f90421ccb1a3
ZouLuX <Zou_LuXian@hollywant.com>
2017-03-23 19:34:54 +0800

    修改了导航栏

--- src/App.js
+++ src/App.js
@@ -7,25 +7,47 @@ import {
     Text,
     View,
     ListView,
-    ToastAndroid
+    ToastAndroid,
+    Image,
 } from 'react-native';
 import {Scene, Router, ActionConst,} from 'react-native-router-flux';
+
 
 import Splash from './pages/Splash'
 import Main from './pages/Main'
 import Details from './pages/Details'
 
-
 export default class ZhihuNews extends Component {
+
+    clickMainLeft(v) {
+        console.log(myIcon);
+    }
+
     render() {
         return (
             <Router key='root'>
                 <Scene key='splash' component={Splash} hideNavBar hideTabBar initial/>
-                <Scene key='main' component={Main} type={ActionConst.REPLACE} title="首页"/>
-                <Scene key='details' component={Details}  title="详情"/>
+                <Scene key='main'
+                       component={Main}
+                       title="首页"
+                       titleStyle={styles.titleStyle}
+                       navigationBarStyle={styles.navBarStyle}
+                       type={ActionConst.REPLACE}
+                />
+                <Scene key='details' component={Details} title="详情" titleStyle={styles.titleStyle}
+                       navigationBarStyle={styles.navBarStyle}/>
             </Router>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    navBarStyle: {
+        backgroundColor: '#00a2ed',
+    },
+    titleStyle: {
+        color: 'white',
+    }
+});
 
 
