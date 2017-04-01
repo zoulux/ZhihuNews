5697f437794dec0b98011a30b6e44c4d9038db3b
ZouLuX <Zou_LuXian@hollywant.com>
2017-04-01 11:25:40 +0800

    添加了加载更多

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -9,26 +9,37 @@ import {
     ListView,
     ToastAndroid,
     RefreshControl,
+    InteractionManager,
 } from 'react-native';
 import ViewPager from 'react-native-viewpager';
+import moment from 'moment';
 
 import NewsItem from '../component/NewsItem'
 import Divider from '../component/Divider'
 import BannerItem from '../component/BannerItem'
 
 const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-var bannerDs = new ViewPager.DataSource({pageHasChanged: (p1, p2) => p1 !== p2});
+const bannerDs = new ViewPager.DataSource({pageHasChanged: (p1, p2) => p1 !== p2});
+
+const date = new Date()
+const str = '' + date.getFullYear() + (date.getMonth() + 1) + (date.getDate());
+
+let beforeDay = 0;
 
 export default class ZhihuNews extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            data: [],
             dataSource: ds.cloneWithRows([]),
             bannerDataSource: bannerDs.cloneWithPages([]),
             refreshing: false,
+            date: '',
         }
-        this.fetchData = this.fetchData.bind(this)
-        this._refreshControl = this._refreshControl.bind(this)
+        this.fetchData = this.fetchData.bind(this);
+        this._refreshControl = this._refreshControl.bind(this);
+        this.setListData = this.setListData.bind(this);
+        this.fetchBeforeData = this.fetchBeforeData.bind(this);
     }
 
     componentDidMount() {
@@ -37,21 +48,48 @@ export default class ZhihuNews extends Component {
 
     _onRefresh() {
         this.fetchData()
+        beforeDay = 0;
     }
 
     fetchData() {
+        this.setState({
+            refreshing: true
+        })
         fetch('http://news-at.zhihu.com/api/4/news/latest')
             .then((response) => response.json())
             .then((responseJson) => {
                 this.setState({
-                    dataSource: this.state.dataSource.cloneWithRows(responseJson.stories),
                     bannerDataSource: this.state.bannerDataSource.cloneWithPages(responseJson.top_stories),
+                    refreshing: false,
                 })
+                //设置dataSource中数据
+                this.setListData(responseJson.stories);
+
             })
             .catch((e) => {
                 alert(e)
             })
     }
+
+    fetchBeforeData() {
+        let time = moment().add(--beforeDay, 'days').format('YYYYMMDD');
+        fetch(`http://news-at.zhihu.com/api/4/news/before/${time}`)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setListData(this.state.data.concat(responseJson.stories));
+            })
+            .catch((e) => {
+                alert(e)
+            })
+    }
+
+    setListData(data) {
+        this.setState({
+            data: data,
+            dataSource: ds.cloneWithRows(data)
+        });
+    }
+
 
     _renderRow(rowData) {
         return <NewsItem data={rowData}></NewsItem>
@@ -83,17 +121,38 @@ export default class ZhihuNews extends Component {
         />;
     }
 
+
+    _onEndReached() {
+        if (this.state.refreshing) {
+            return;
+        }
+        if (this.state.dataSource.getRowCount() < 5) {
+            return;
+        }
+
+
+        InteractionManager.runAfterInteractions(() => {
+            this.fetchBeforeData();
+        })
+
+    }
+
     // renderRow 每一行Item
     // renderSeparator 分割线
+    //initialListSize 组件第一次加载时需要渲染的行数
+    //onEndReachedThreshold 滑动距离底部还有n单位像素时会触发 onEndReached 方法
     render() {
         return (
             <View style={styles.container}>
                 <ListView
                     style={styles.container}
+                    enableEmptySections={true}
+                    onEndReachedThreshold={20}
+                    initialListSize={10}
                     dataSource={this.state.dataSource}
                     renderRow={this._renderRow.bind(this)}
-                    enableEmptySections={true}
                     renderHeader={this._renderHeader.bind(this)}
+                    onEndReached={ this._onEndReached.bind(this) }
                     refreshControl={<RefreshControl
                         refreshing={this.state.refreshing}
                         colors={['#ff0000', '#00ff00', '#0000ff']}
